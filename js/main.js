fd093a30f9e9cae09e477167a996e0e76ac705cd
da.ivanov <da.ivanov@tensor.ru>
2018-02-06 17:50:46 +0300

    Вставка картинки через шаблон

--- js/main.js
+++ js/main.js
@@ -3,72 +3,88 @@ Box = Backbone.Model.extend({
 })
 
 BoxCollection = Backbone.Collection.extend({
-  model: Box
+   model: Box
 })
 
 BoxView = Backbone.View.extend({
-  tagName: 'img',
-  initialize: function() {
-    _.bindAll(this, 'render');
-    this.model.bind('change', this.render);
-    this.model.view = this;
-  },
-  render: function(){
-    console.log('BoxViewRender');
-    this.el.src = this.model.get('src');
-    this.el.height = 300;
-    this.el.width = 300;
-    return this;
-  }
+   events: {
+      'load': 'setImageSize'
+   },
+   template: _.template($('#boxTemplate').html()),
+   initialize: function() {
+      _.bindAll(this, 'render');
+      this.model.bind('change', this.render);
+      this.model.view = this;
+   },
+   render: function() {
+      this.$el.html(this.template(this.model));
+      this.$('img').hide().one('load', this.setImageSize.bind(this)); // Скрываем картинку пока не установим ей разер, чтобы не дергалось
+      return this;
+   },
+   setImageSize: function() {
+      // Делаем максимальную велечину равной 500px
+      var MAX_VAL = 500;
+      var img = this.$('img');
+
+      if (img.width() > img.height() && img.width() > MAX_VAL) {
+         img.width(MAX_VAL);
+      } else if (img.height() > MAX_VAL) {
+         img.height(MAX_VAL);
+      }
+      img.show();
+
+      return this;
+   }
 })
 
 MainView = Backbone.View.extend({
-  el: $('#main'),
-  events: {
-    'click #addImage': 'addImage',
-    'click #addVideo': 'addVideo',
-    'change #addImageInput': 'readURL'
-  },
-  initialize: function() {
-    this.collection.on('add', this.addVideo, this);
-  },
-  render: function() {
-    console.log('MainViewRender');
-    this.collection.each(this.addVideo, this);
-    return this;
-  },
-  addImage: function(box) {
-    this.$('#addImageInput').trigger('click');
-  },
-  addVideo: function(box) {
-    console.log('addVideo');
-    var boxView = new BoxView({model: box});
-    this.$('#container').append($(boxView.render().el).draggable())
-  },
-  readURL: function(event) {
-    var input = event.target;
-    var self = this;
+   el: $('#main'),
+   events: {
+      'click #addImage': 'addImage',
+      'click #addVideo': 'addVideo',
+      'change #addImageInput': 'readURL'
+   },
+   initialize: function() {
+      this.collection.on('add', this.addBox, this);
+   },
+   render: function() {
+      this.collection.each(this.addBox, this);
+      return this;
+   },
+   addImage: function() {
+      this.$('#addImageInput').trigger('click');
+   },
+   addVideo: function() {
+      this.$('#addImageInput').trigger('click');
+   },
+   addBox: function(box) {
+      var boxView = new BoxView({
+         model: box
+      });
+      this.$('#container').append($(boxView.render().el).draggable())
+   },
+   readURL: function(event) {
+      var input = event.target;
+      var self = this;
 
-    if (input.files && input.files[0] && input.files[0].type.includes('image')) {
-      var reader = new FileReader();
+      if (input.files && input.files[0] && input.files[0].type.includes('image')) {
+         var reader = new FileReader();
 
-      reader.onload = function(e) {
-        self.collection.add( new Box({
-          src: e.target.result
-        }))
-        // $('#blah')
-        //   .attr('src', e.target.result)
-        //   .width(300)
-        //   .height(300);
-      };
+         reader.onload = function(e) {
+            self.collection.add(new Box({
+               src: e.target.result
+            }))
+         };
 
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
+         reader.readAsDataURL(input.files[0]);
+      }
+   }
 })
 
-Boxes = new BoxCollection([
-  {src: 'https://yt3.ggpht.com/-dcBh0pHnxS8/AAAAAAAAAAI/AAAAAAAAAAA/IjOqcL9bAXg/s88-c-k-no-mo-rj-c0xffffff/photo.jpg'}
-]);
-App = new MainView({collection: Boxes});
+boxes = new BoxCollection([{
+   src: 'https://yt3.ggpht.com/-dcBh0pHnxS8/AAAAAAAAAAI/AAAAAAAAAAA/IjOqcL9bAXg/s88-c-k-no-mo-rj-c0xffffff/photo.jpg'
+}]);
+App = new MainView({
+   collection: boxes
+});
 App.render();
