45035e3fc2f0c6aa8b608f035b9dbf56f5c0b95e
da.ivanov <da.ivanov@tensor.ru>
2018-02-07 16:56:22 +0300

    Небольшик правки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 Box = Backbone.Model.extend({
-
+   defaults: {
+      "aspectRatio": false,
+      "top": 0,
+      "left": 0,
+      "height": 500,
+      "width": 500
+   }
 })
 
 BoxCollection = Backbone.Collection.extend({
@@ -13,8 +19,6 @@ BoxView = Backbone.View.extend({
    className: 'box_draggable box_grey',
    template: _.template($('#boxTemplate').html()),
    initialize: function() {
-      _.bindAll(this, 'render');
-      this.model.bind('change', this.render);
       this.model.view = this;
    },
    render: function() {
@@ -29,10 +33,10 @@ BoxView = Backbone.View.extend({
       var img = this.$('img')[0];
 
       // Устанавливаем новые размеры на основе MAX_VAL
-      if (img.width > img.height && img.width > MAX_VAL){
+      if (img.width >= img.height && img.width > MAX_VAL) {
          width = MAX_VAL;
          height = img.height * (MAX_VAL / img.width);
-      } else if (img.height > img.width && img.height > MAX_VAL){
+      } else if (img.height > img.width && img.height > MAX_VAL) {
          height = MAX_VAL;
          width = img.width * (MAX_VAL / img.height);
       } else {
@@ -40,10 +44,16 @@ BoxView = Backbone.View.extend({
          width = img.width;
       }
 
+      this.model.set('height', height);
+      this.model.set('width', width);
+
       // Делаем серый блок размером с картинку
       // С анимацией чтобы не было дёргания
-      this.$el.animate({height: height, width: width}, 150)
-      // this.$el.width(width).height(height);
+      this.$el.animate({
+            height: height,
+            width: width
+         }, 150)
+         // this.$el.width(width).height(height);
 
       this.$('img').fadeIn();
 
@@ -71,14 +81,18 @@ MainView = Backbone.View.extend({
    addVideo: function() {
       var rickRoll = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
       var url = prompt("Please enter youtube video URL", rickRoll);
-      var videoId = url.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop();
 
-      if (videoId.length === 11) {
-         var thumbnail = 'https://img.youtube.com/vi/' + videoId + '/0.jpg';
-         this.collection.add(new Box({
-            src: thumbnail,
-            aspectRatio: true
-         }))
+      if (url) {
+         // Получение ID видео из ссылки
+         var videoId = url.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop();
+
+         if (videoId.length === 11) {
+            var thumbnail = 'https://img.youtube.com/vi/' + videoId + '/0.jpg';
+            this.collection.add(new Box({
+               src: thumbnail,
+               aspectRatio: true
+            }))
+         }
       }
    },
    addBox: function(box) {
@@ -86,13 +100,13 @@ MainView = Backbone.View.extend({
          model: box
       });
       this.$('#container').append($(boxView.render().el).draggable().resizable({
-        aspectRatio: box.get('aspectRatio'),
-        handles: {
-          'ne': '#negrip',
-          'se': '#segrip',
-          'sw': '#swgrip',
-          'nw': '#nwgrip'
-        }
+         aspectRatio: box.get('aspectRatio'),
+         handles: {
+            'ne': '#negrip',
+            'se': '#segrip',
+            'sw': '#swgrip',
+            'nw': '#nwgrip'
+         }
       }))
    },
    readURL: function(event) {
@@ -118,4 +132,3 @@ boxes = new BoxCollection();
 App = new MainView({
    collection: boxes
 });
-App.render();
